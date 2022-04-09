4ad5c9ad1fb95e97232fd1948b41be69c4ef21af
Eric Thornton <eric.thornton.2000@gmail.com>
2022-04-09 13:19:14 -0400

    Wired up all the buttons

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Global variables
+
+let firstNum = '';
+let secondNum = undefined;
+let operation = '';
+const display = document.querySelector('.display > h1');
+
+// Calculator functions
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -7,7 +16,7 @@ function subtract(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if(num2 == 0) {return "error"}
+    if(num2 == 0) {return 'error'}
     return num1 / num2
 }
 
@@ -15,13 +24,71 @@ function multiply(num1, num2) {
     return num1 * num2
 }
 
-function evaluate(num1, num2, operation) {
-    let num3 = operation(num1, num2);
-    console.log(num3);
+function operationHandler(operation) {
+    switch(operation) {
+        case '+':
+            return add;
+        case '-':
+            return subtract;
+        case '/':
+            return divide;
+        case '*':
+            return multiply;
+    }
 }
 
-evaluate(3, 9, multiply);
-evaluate(3, 9, add);
-evaluate(3, 0, divide);
-evaluate(3, 10, divide);
-evaluate(3, 9, subtract);
+function evaluate(num1, num2, operationFunction) {
+    firstNum = (operationFunction(num1, num2));
+    secondNum = undefined;
+    operation = '';
+    console.log(`operation reset to ${operation}`);
+    display.innerText = firstNum;
+}
+
+// Event listeners for the buttons
+const buttons = document.querySelectorAll('.numpad > .btn');
+const operations = document.querySelectorAll('.operations > .btn');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        console.log(event.target.innerText);
+        if(operation == '') {
+            display.innerText += event.target.innerText;
+            firstNum += (event.target.innerText);
+            console.log('firstNum = ' + firstNum);
+        } else if (secondNum === undefined) {
+            display.innerText += ' ' + event.target.innerText;
+            secondNum = event.target.innerText;
+            console.log('secondNum = ' + secondNum);
+        } else {
+            display.innerText += event.target.innerText;
+            secondNum += event.target.innerText;
+            console.log('secondNum = ' + secondNum);
+        }
+    });
+});
+
+operations.forEach(button => {
+    button.addEventListener('click', event => {
+        if(operation == '') {
+            operation = event.target.textContent;
+        }
+        display.textContent += ` ${operation} `;
+        console.log(`secondNum = ${secondNum}`);
+        if(secondNum != undefined) {
+            evaluate(Number(firstNum), Number(secondNum), operationHandler(operation));
+        }
+    })
+});
+
+equals.addEventListener('click', event => {
+    evaluate(Number(firstNum), Number(secondNum), operationHandler(operation));
+});
+
+clear.addEventListener('click', event => {
+    firstNum = '';
+    secondNum = undefined;
+    operation = '';
+    display.innerText = '';
+});
